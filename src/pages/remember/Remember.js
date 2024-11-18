f91959c36e1fbc7d51cef752f4d1f769e0e0d308
marcelleengle <47188835+marcelleengle@users.noreply.github.com>
2024-11-18 19:34:21 -0300

    Create Remember.js

---
+++ src/pages/remember/Remember.js
@@ -0,0 +1,27 @@
+import '../../App.css';
+import React from "react";
+import Photos from "./photos";
+
+
+
+function Remember() {
+  return (
+    <>
+      <Photos></Photos>
+      <div>
+        <p>No próximo dia 27 de novembro a diva comemora seus 60 anos.</p>
+        <p>E a gata parece que descobriu o segredo da juventude, pq né... as fotos falam por si só!</p>
+        <p>Amante dos animais, apaixonada pelos bichinhos (de quatro patas, duas asas, sem patas)</p>
+        <p>A casa parece um zoologico com tanto bicho</p>
+        <p>Isso sem contar as plantas</p>
+        <p>Um sagitariana paz e amor, good vibes, até tirarem ela do sério... Mexe com ela pra tu ver se tu não leva uma patada bem dada.</p>
+        <p>Mãe de dois filhos (humanos) e casa com Andre, uma cozinheira de mão cheia, tudo que faz fica bom.</p>
+        <p>Sem contar as habilidades pra artesanato que a gata contruiu ao longo dos anos</p>
+        <p>Amante de café, meio esquecida as vezes (pode ser que ela esqueça de puxar o freio de mão do carro), pilateira. Não queira nunca vê-la com fome!</p>
+        <p>Enfim... ta aí um pouco da nossa aniversariante!</p>
+      </div>
+    </>
+  );
+}
+
+export default Remember;
