c810310547da5ef209761d8aac39c3353f059532
marcelleengle <47188835+marcelleengle@users.noreply.github.com>
2024-11-18 19:35:53 -0300

    Create Clima.js

---
+++ src/pages/clima/Clima.js
@@ -0,0 +1,13 @@
+import '../../App.css';
+import React from "react";
+import Climaconfig from "./climaconfig";
+
+
+
+function Clima() {
+  return (
+      <Climaconfig></Climaconfig>
+  );
+}
+
+export default Clima;
